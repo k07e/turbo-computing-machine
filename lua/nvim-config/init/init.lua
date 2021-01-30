ca363db70680e8a6b100ee03c3d68afd3bea7e13
k07e <sakk7e+dev@gmail.com>
2021-01-30 11:45:14 +0900

    Close #4

--- lua/nvim-config/init/init.lua
+++
@@ -1,4 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-vim.api.nvim_command("set cursorline")
-vim.api.nvim_command("set number")
-vim.api.nvim_command("set relativenumber")
